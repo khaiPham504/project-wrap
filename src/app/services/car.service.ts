4527d345680f560c877f096088d6d89b8817e7fb
manhvdo95 <manhvdo95@yahoo.com>
2025-06-04 20:32:15 -0500

    update image path

--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -8,43 +8,43 @@ export class CarService {
 
   cars = [
     {
-      'src': '/assets/cars/dark_red_toyota_camery.jpeg'
+      'src': 'assets/cars/dark_red_toyota_camery.jpeg'
     },
     {
-      'src': '/assets/cars/black_benz.jpeg'
+      'src': 'assets/cars/black_benz.jpeg'
     },
     {
-      'src': '/assets/cars/yellow_bmw.jpeg'
+      'src': 'assets/cars/yellow_bmw.jpeg'
     },
     {
-      'src': '/assets/cars/carbon-honda.jpeg'
+      'src': 'assets/cars/carbon-honda.jpeg'
     }
   ];
 
   categoryCars = [
     {
       'category': 'gloss',
-      'src': '/assets/vinyl/gloss.png'
+      'src': 'assets/vinyl/gloss.png'
     },
     {
       'category': 'matte',
-      'src': '/assets/vinyl/matte.png'
+      'src': 'assets/vinyl/matte.png'
     },
     {
       'category': 'satin',
-      'src': '/assets/vinyl/satin.png'
+      'src': 'assets/vinyl/satin.png'
     },
     {
       'category': 'chrome',
-      'src': '/assets/vinyl/chrome.png'
+      'src': 'assets/vinyl/chrome.png'
     },
     {
       'category': 'color-shift',
-      'src': '/assets/vinyl/color-shift.png'
+      'src': 'assets/vinyl/color-shift.png'
     },
     {
       'category': 'carbon',
-      'src': '/assets/vinyl/carbon.png'
+      'src': 'assets/vinyl/carbon.png'
     }
   ];
 
